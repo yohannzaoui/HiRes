a29e8ed5099719a498014524ba832ccd2cd167a8
Yohann Zaoui <yohannzaoui@gmail.com>
2026-01-11 07:55:40 +0100

    add PWA

---
+++ sw.js
@@ -0,0 +1,16 @@
+const CACHE_NAME = 'hifi-player-v1';
+const ASSETS = [
+  'index.html',
+  'style.css',
+  'script.js',
+  'https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css',
+  'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css'
+];
+
+self.addEventListener('install', (e) => {
+  e.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS)));
+});
+
+self.addEventListener('fetch', (e) => {
+  e.respondWith(caches.match(e.request).then(response => response || fetch(e.request)));
+});
